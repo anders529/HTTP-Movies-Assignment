f071c951ccc8a2c1ab49060121a8f44f61260a92
Aaron Andersen <anderseana@tutanota.com>
2020-01-30 14:00:48 -0600

    Got everything to work flawlessly! MVP is met!

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -7,32 +7,34 @@ export default class Movie extends React.Component {
       {super(props);
       this.state = {movie: null}
       };
-
   componentDidMount() {this.fetchMovie(this.props.match.params.id)};
-
   componentWillReceiveProps(newProps) {
     if (this.props.match.params.id !== newProps.match.params.id) {
       this.fetchMovie(newProps.match.params.id);
     }}
-
   fetchMovie = id => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(res => this.setState({ movie: res.data }))
       .catch(err => console.log(err.response))};
-
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie)};
-
+          addToSavedList(this.state.movie)};
+  deleteMovie = () => {
+      axios.delete(`http://localhost:5000/api/movies/${this.state.movie.id}`)
+          .then(response => {
+              console.log(response);
+              this.props.history.push('/')})};
   render() {
     if (!this.state.movie) {return <div>Loading movie information...</div>;}
     return (
-      <div className="save-wrapper">
-        <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>
-          Save
-        </div>
-          <Link to={`/update-movie/${this.state.movie.id}`}>Add Movie</Link>
-      </div>
+          <div className="save-wrapper">
+              <MovieCard movie={this.state.movie}/>
+              <div className="save-button" onClick={this.saveMovie}>
+                  Save
+              </div>
+              <Link to={`/update-movie/${this.state.movie.id}`}>Update Movie</Link>
+                <br/>
+              <button onClick={this.deleteMovie}>Delete Movie</button>
+          </div>
     )}};
